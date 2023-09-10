1d25dd00d06f39f396f349f964a77de8af20b325
robingotz <tug29225@temple.edu>
2023-09-10 13:14:13 +0200

    feat: setup spartan tw preset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<p>Hello world</p>`,
+  host: {
+    class: 'block bg-background text-foreground p-4'
+  },
+  template: `
+    <p>Hello world</p>
+    `
 })
 export class AppComponent {
 }
